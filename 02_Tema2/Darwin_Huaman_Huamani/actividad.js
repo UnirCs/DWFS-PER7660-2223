7016cf5593f8dffc7f0e734d1f10995ef6440b49
MartinDarwin <60305807+MartinDarwin@users.noreply.github.com>
2023-04-04 22:34:36 -0500

    Update actividad.js

--- 02_Tema2/Darwin_Huaman_Huamani/actividad.js
+++ 02_Tema2/Darwin_Huaman_Huamani/actividad.js
@@ -285,7 +285,7 @@ function merge(alphaFirst, alphaSecond) {
  *     Negativo: 8
  *     Fusion de imagenes: 9
  */
-let optionN = 3;
+let optionN = 1;
 
 switch (optionN) {
     case 1: redConverter(); break;
