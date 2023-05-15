8b2d4573d95146297eed49cfd575729bf6af4b5e
Patogol35 <128660919+Patogol35@users.noreply.github.com>
2023-05-15 11:27:00 -0500

    Add files via upload

---
+++ 05_Tema5/Jorge_Patricio_Santamaria_Cherrez_Ejercicio_Tema5_Cards/master_dashboard_Ejercicio_5_Jorge_Patricio_Santamaría_Cherrez/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './componente/Card';
+import './estilos.css';
+function App() {
+  return (
+    <div>
+      <h1 className='título'>Máster de Ingeniería de Software y Sistemas Informáticos</h1>
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
