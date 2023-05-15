44650be5051e95ac9d1cc59d9ab1ab5e9c214d0d
Patogol35 <128660919+Patogol35@users.noreply.github.com>
2023-05-15 14:49:21 -0500

    Add files via upload

--- 05_Tema5/Jorge_Patricio_Santamaria_Cherrez_Ejercicio_Tema5_Cards/master_dashboard_Ejercicio_5_Jorge_Patricio_Santamaría_Cherrez/src/componente/Card.js
+++ 05_Tema5/Jorge_Patricio_Santamaria_Cherrez_Ejercicio_Tema5_Cards/master_dashboard_Ejercicio_5_Jorge_Patricio_Santamaría_Cherrez/src/componente/Card.js
@@ -1,51 +1,18 @@
-import React from 'react';
+
+
+import React from "react";
 import logo1 from '../imagenes/1.jpg';
-import logo2 from '../imagenes/2.jpg';
-import logo3 from '../imagenes/3.jpg';
-import logo4 from '../imagenes/4.png';
-import logo5 from '../imagenes/5.jpg';
-import logo6 from '../imagenes/6.png';
-import logo7 from '../imagenes/7.jpg';
-function Card() {
+
+
+export const Card = ({  asignatura, abreviatura, estilos }) => {
   return (
-    <div>
-    <div className='primercuatrimestre1'>
-      <h2>Desarrollo Web Full Stack</h2>
-      <p>DWFS</p>
-      <img src={logo4}  alt='logo1' className="logo" />
-    </div>
-    <div className='primercuatrimestre2'>
-      <h2>Dirección y Gestión de proyectos de Software</h2>
-      <p>DGPS</p>
-      <img src={logo5}  alt='logo2' className="logo" />
-    </div> 
-    <div className='primercuatrimestre3'>
-      <h2>Plataformas de Desarrollo de Software</h2>
-      <p>PDS</p>
-      <img src={logo2}  alt='logo3' className="logo" />
-    </div>
-    <div className='primercuatrimestre4'>
-      <h2>Ciberseguridad web</h2>
-      <p>CW</p>
-      <img src={logo3}  alt='logo4' className="logo" />
-    </div>
-    <div className='primercuatrimestre5'>
-      <h2>Metodologías, Desarrollo y Calidad en la Ingeniería del Software</h2>
-      <p>MDCIS</p>
-      <img src={logo6}  alt='logo5' className="logo" />
-    </div>
-    <div className='segundocuatrimestre1'>
-      <h2>Prácticas en Empresa</h2>
-      <p>PE</p>
-      <img src={logo1}  alt='logo6' className="logo" />
-    </div>
-    <div className='segundocuatrimestre2'>
-      <h2>Trabajo Fin de Máster</h2>
-      <p>TFM</p>
-      <img src={logo7}  alt='logo7' className="logo" />
-    </div>
+    <div className={estilos}>
+  
+      <h2>{asignatura}</h2>
+      <h2>{abreviatura}</h2>
+      <img src={logo1} alt="img" className="logo"/>
+    
     </div>
   );
-}
-
+};
 export default Card;
