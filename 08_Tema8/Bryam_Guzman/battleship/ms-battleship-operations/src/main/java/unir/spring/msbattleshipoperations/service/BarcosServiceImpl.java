bc785ea0581044a05fabc35f9a9bea6eeb39daac
Bryam Guzmán <bguzman@vrgroup.cl>
2023-06-19 22:42:05 -0500

    Validaciones creacion barcos

--- 08_Tema8/Bryam_Guzman/battleship/ms-battleship-operations/src/main/java/unir/spring/msbattleshipoperations/service/BarcosServiceImpl.java
+++ 08_Tema8/Bryam_Guzman/battleship/ms-battleship-operations/src/main/java/unir/spring/msbattleshipoperations/service/BarcosServiceImpl.java
@@ -34,6 +34,10 @@ public class BarcosServiceImpl implements BarcosService {
                 posicionesFin(barcoRequest.getPosicionesFin()).idJugadorAsociado(Long.valueOf(idJugador)).
                 partida(partida).orientacion(barcoRequest.getOrientacion()).build();
 
+        if (!validarBarco(barco)) {
+            throw new IllegalArgumentException("Barco no válido");
+        }
+
         return barcoRepository.save(barco);
     }
 
@@ -50,4 +54,153 @@ public class BarcosServiceImpl implements BarcosService {
         }
 
     }
+
+
+    private boolean validarBarco(Barco barco) {
+
+        if (!validarTipoBarco(barco)) {
+            return false;
+        }
+
+        if (!validarPosicionesBarco(barco)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validarTipoBarco(Barco barco) {
+        String tipo = barco.getTipo();
+        return tipo.equals("barco4") || tipo.equals("barco3") || tipo.equals("barco2") || tipo.equals("barco1");
+    }
+
+    private boolean validarPosicionesBarco(Barco barco) {
+        List<Double> posicionesInicio = barco.getPosicionesInicio();
+        List<Double> posicionesFin = barco.getPosicionesFin();
+
+        if (posicionesInicio.size() != 2 || posicionesFin.size() != 2) {
+            return false;
+        }
+
+        double minX = 0.0;
+        double maxX = 10.0;
+        double minY = 0.0;
+        double maxY = 10.0;
+
+        double posXInicio = posicionesInicio.get(0);
+        double posYInicio = posicionesInicio.get(1);
+        double posXFin = posicionesFin.get(0);
+        double posYFin = posicionesFin.get(1);
+
+        if (posXInicio < minX || posXInicio > maxX || posYInicio < minY || posYInicio > maxY ||
+                posXFin < minX || posXFin > maxX || posYFin < minY || posYFin > maxY) {
+            return false;
+        }
+
+        if (!validarColocacionBarco(barco.getPartida().getId(), barco.getIdJugadorAsociado(),
+                posicionesInicio, posicionesFin)) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    private boolean validarColocacionBarco(Long idPartida, Long idJugadorAsociado,
+                                           List<Double> posicionesInicio, List<Double> posicionesFin) {
+
+        double posXInicio = posicionesInicio.get(0);
+        double posYInicio = posicionesInicio.get(1);
+        double posXFin = posicionesFin.get(0);
+        double posYFin = posicionesFin.get(1);
+
+        boolean esHorizontal = posXInicio != posXFin;
+        boolean esVertical = posYInicio != posYFin;
+
+        if (!(esHorizontal || esVertical)) {
+            return false;
+        }
+
+        int longitudBarco = 0;
+        if (esHorizontal) {
+            longitudBarco = Math.abs((int) (posXFin - posXInicio));
+        } else if (esVertical) {
+            longitudBarco = Math.abs((int) (posYFin - posYInicio));
+        }
+
+        if (longitudBarco < 1 || longitudBarco > 4) {
+            return false;
+        }
+
+        List<Barco> barcosEnPartida = barcoRepository.findByIdJugadorAsociadoAndPartidaId(idJugadorAsociado, idPartida);
+
+        for (Barco barcoExistente : barcosEnPartida) {
+            if (seSuperponen(posicionesInicio, posicionesFin, barcoExistente.getPosicionesInicio(), barcoExistente.getPosicionesFin())) {
+                return false;
+            }
+        }
+
+        if (!respetaFranjaCuadrosEnBlanco(barcosEnPartida, posicionesInicio, posicionesFin)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean seSuperponen(List<Double> posicionesInicio1, List<Double> posicionesFin1,
+                                 List<Double> posicionesInicio2, List<Double> posicionesFin2) {
+        double posXInicio1 = posicionesInicio1.get(0);
+        double posYInicio1 = posicionesInicio1.get(1);
+        double posXFin1 = posicionesFin1.get(0);
+        double posYFin1 = posicionesFin1.get(1);
+
+        double posXInicio2 = posicionesInicio2.get(0);
+        double posYInicio2 = posicionesInicio2.get(1);
+        double posXFin2 = posicionesFin2.get(0);
+        double posYFin2 = posicionesFin2.get(1);
+
+        boolean seSuperponenHorizontalmente = (posYInicio1 == posYInicio2 || posYInicio1 == posYFin2) &&
+                ((posXInicio1 >= posXInicio2 && posXInicio1 <= posXFin2) || (posXFin1 >= posXInicio2 && posXFin1 <= posXFin2));
+
+        boolean seSuperponenVerticalmente = (posXInicio1 == posXInicio2 || posXInicio1 == posXFin2) &&
+                ((posYInicio1 >= posYInicio2 && posYInicio1 <= posYFin2) || (posYFin1 >= posYInicio2 && posYFin1 <= posYFin2));
+
+        if (seSuperponenHorizontalmente || seSuperponenVerticalmente) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    private boolean respetaFranjaCuadrosEnBlanco(List<Barco> barcosEnPartida,List<Double> posicionesInicio,
+                                                 List<Double> posicionesFin) {
+
+        double posXInicio = posicionesInicio.get(0);
+        double posYInicio = posicionesInicio.get(1);
+        double posXFin = posicionesFin.get(0);
+        double posYFin = posicionesFin.get(1);
+
+        double minX = posXInicio - 1;
+        double maxX = posXFin + 1;
+        double minY = posYInicio - 1;
+        double maxY = posYFin + 1;
+
+        for (Barco barcoExistente : barcosEnPartida) {
+            List<Double> posicionesInicioExistente = barcoExistente.getPosicionesInicio();
+            List<Double> posicionesFinExistente = barcoExistente.getPosicionesFin();
+
+            double posXInicioExistente = posicionesInicioExistente.get(0);
+            double posYInicioExistente = posicionesInicioExistente.get(1);
+            double posXFinExistente = posicionesFinExistente.get(0);
+            double posYFinExistente = posicionesFinExistente.get(1);
+
+            if (posXInicioExistente >= minX && posXFinExistente <= maxX && posYInicioExistente >= minY && posYFinExistente <= maxY) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
